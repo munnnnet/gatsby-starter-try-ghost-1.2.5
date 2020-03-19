26a8f0ef1f4756304c36e4308d96be4ac9b87c67
Fatul <33377537+KontenaIndonesia@users.noreply.github.com>
2020-03-19 22:29:59 +0700

    Update siteConfig.js

--- siteConfig.js
+++ siteConfig.js
@@ -1,15 +1,15 @@
 module.exports = {
-    siteUrl: `https://styxlab.github.io`, // Site domain. Do not include a trailing slash!
+    siteUrl: `https://metricxl.com`, // Site domain. Do not include a trailing slash!
 
     postsPerPage: 12, // Number of posts shown on paginated pages (changes this requires sometimes to delete the cache)
 
-    siteTitleMeta: `Gatsby Ghost Casper`, // This allows an alternative site title for meta data for pages.
-    siteDescriptionMeta: `Gastby with Ghost CMS and Casper Theme`, // This allows an alternative site description for meta data for pages.
+    siteTitleMeta: `Metrics and Stats Nerd`, // This allows an alternative site title for meta data for pages.
+    siteDescriptionMeta: Collects marketing metrics and stats across the business niche. Help marketing level up to the next stage.', // This allows an alternative site description for meta data for pages.
 
     shareImageWidth: 1000, // Change to the width of your default share image
     shareImageHeight: 523, // Change to the height of your default share image
 
-    shortTitle: `Ghost`, // Used for App manifest e.g. Mobile Home Screen
+    shortTitle: `metricXL.com`, // Used for App manifest e.g. Mobile Home Screen
     siteIcon: `favicon.png`, // Logo in /static dir used for SEO, RSS, and App manifest
     backgroundColor: `#e9e9e9`, // Used for Offline Manifest
     themeColor: `#15171A`, // Used for Offline Manifest
